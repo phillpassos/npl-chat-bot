da08d89376b8a2192fa875a4617453c55736ba4a
Phillip Miguel dos Passos <phillipmigueldospassos@gmail.com>
2017-12-09 12:37:23 -0200

    correcao do arquivo

--- nplbot.py
+++ nplbot.py
@@ -1,31 +1,39 @@
-#chatbot from https://chatbotsmagazine.com/contextual-chat-bots-with-tensorflow-4391749d0077
 
-
-# coisas da NPL
+# coisas da NLP
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-# coisas do Tensorflow
+# Tensorflow e numpy
 import numpy as np
 import tflearn
 import tensorflow as tf
+
 import random
 
 
-# arquivos dos intents
+# arquivo datasource
 import json
+
 with open('intents.json', mode='r', buffering=-1, encoding="UTF-8") as json_data:
     intents = json.load(json_data)
 
+# lista de palavras nos dados (palavras unicas)
 words = []
+
+# lista da sub categoria que determinara o resultado
 classes = []
+
+# lista de palavras relacionadas aos padrões
 documents = []
+
+# itens que seram ignorados no modelo
 ignore_words = ['?', '!']
+
 # loop das sentencas do arquivo e padroes
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        # gerando os tokens dos padroes
+        # quebrando os patterns em arrays
         w = nltk.word_tokenize(pattern)
         # adicionando a nossa lista de palavras
         words.extend(w)
@@ -36,15 +44,22 @@ for intent in intents['intents']:
             classes.append(intent['tag'])
 
 # filtrando as palavras
+# o stemmer nao funciona bem em portugues, mas ja ajuda pra gerar palavras de forma mais exclusiva
 words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+
+# ordeno alfabeticamente e removo duplicatas das palavras
 words = sorted(list(set(words)))
 
-# remocao de duplicatas
+# ordeno alfabeticamente e removo duplicatas das classes (tag)
 classes = sorted(list(set(classes)))
 
-print (len(documents), "documents")
+print("|")
+print("----------------------------------------")
+print("Fim do tratamento dos dados: ")
+print (len(documents), "documentos")
 print (len(classes), "classes", classes)
-print (len(words), "unique stemmed words", words)
+print (len(words), "palavras unicas", words)
+print("----------------------------------------")
 
 
 
@@ -52,62 +67,75 @@ print (len(words), "unique stemmed words", words)
 
 
 # criando dados de treinamento
+
 training = []
 output = []
-# criando array vazio pra entrada
+
+# criando array vazio pra saida do tamanho da quantidade de classes
 output_empty = [0] * len(classes)
 
 # conjunto de trainamento, pack de palavras pra cada sentenca
 for doc in documents:
-    # inicializa o pack de palavras
     bag = []
-    # lista de palavras tokenizadas para os padroes
+    # lista de palavras para os padroes
     pattern_words = doc[0]
+
     # filtrando palavras
+    # uso novamente o stem
     pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-    # array do pack de palavras
+
+    # array que representa os dados (palavras existentes no documento, vulgo tag)
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
-    # 0 para cada tag e '1' para a tag atual
+    # array que representa o documento (tag!)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
 
+    # aqui relacionamos a representacao em binario das nossas palavras com os documentos
     training.append([bag, output_row])
 
-# misturando os recursos e passando para o np.array
+# misturando os recursos e passando para array do numpy
 random.shuffle(training)
 training = np.array(training)
 
-# criacao da lista de trainemanto e testes
+# criacao da lista de trainemanto[,0] e testes [, 1]
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 
 
+print("|")
+print("----------------------------------------")
+print("Tratamento dos dados de treinamento concluido: ")
+print (len(train_x), "conjuntos para processar")
+print("----------------------------------------")
 
 
 
 
 
-# reset dos dados do grafico
+# reset do grafo, so pra garantir
 tf.reset_default_graph()
+
 # construcao da rede neural
 net = tflearn.input_data(shape=[None, len(train_x[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+# GD
 net = tflearn.regression(net)
 
 # modelo e config do tensorboard
-model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+model = tflearn.DNN(net)
 # iniciar treinamento (GD alg)
-model.fit(train_x, train_y, n_epoch=3000, batch_size=16, show_metric=False)
+model.fit(train_x, train_y, n_epoch=6000, batch_size=32)
+
 model.save('model.tflearn')
 
 
 
 
-# salvar estruturas de dados
+# salvar estruturas de dados, util se a quantidade for grande
 import pickle
 pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
 
@@ -116,7 +144,7 @@ pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':tra
 
 
 
-#----------------------- outro arquivo ----------------------------#
+#----------------------- levar para outro arquivo ----------------------------#
 
 
 
@@ -129,7 +157,7 @@ classes = data['classes']
 train_x = data['train_x']
 train_y = data['train_y']
 
-# importar arquivos de intent do bot
+# importar arquivos de intent > adicionar verificacao para caso o mesmo nao exista
 import json
 with open('intents.json', mode='r', buffering=-1, encoding="UTF-8") as json_data:
     intents = json.load(json_data)
@@ -149,7 +177,7 @@ model.load('./model.tflearn')
 
 
 def clean_up_sentence(sentence):
-    # gerando token dos padroes
+    # separando a sentenca num array
     sentence_words = nltk.word_tokenize(sentence)
     # normalizando palavras
     sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
@@ -157,7 +185,6 @@ def clean_up_sentence(sentence):
 
 # retornando um array do pack de palavras: 0 ou 1 pra cada palavra no pack que existe na sentenca
 def bow(sentence, words, show_details=False):
-    # tokens dos padroes
     sentence_words = clean_up_sentence(sentence)
     # pack de palavras
     bag = [0]*len(words)  
@@ -166,7 +193,7 @@ def bow(sentence, words, show_details=False):
             if w == s: 
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
+                    print ("encontrado: %s" % w)
 
     return(np.array(bag))
 
@@ -219,7 +246,7 @@ def response(sentence, userID='123', show_details=False):
 
             results.pop(0)
 
-
+exit(0)
 # criacao do servidor com o flask
 from flask import Flask
 from flask import request
